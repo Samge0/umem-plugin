bc361c1b1901659c2c6c9c12d02d1b33a545f078
Samge0 <948331124@qq.com>
2022-01-28 22:50:05 +0800

    UMEM助手-一键将友盟登录状态同步更新为UMEM配置的chrome插件

---
+++ README.md
@@ -0,0 +1,58 @@
+![](https://img.shields.io/github/release/samge0/umem-plugin)
+![](https://img.shields.io/github/last-commit/samge0/umem-plugin)
+![](https://img.shields.io/github/issues/samge0/umem-plugin)
+![](https://img.shields.io/github/license/samge0/umem-plugin)
+
+# UMEM-友盟登录助手
+
+> 一键将友盟登录状态同步更新为 [UMEM](https://github.com/Samge0/UmengEventManage.git) 配置的chrome插件（该插件目前仅支持chrome浏览器）。<br><br>UMEM = UmengEventManage，<br>友盟统计自定义事件多应用一键同步 & 批量处理工具。
+
+![UMEM-友盟登录助手](screenshots/umem-h1.png)
+
+
+## 使用
+
+- 1、下载"UMEM助手" 或者 拉取本项目源码编译（编译后会生成dist目录，在浏览器中"加载已解压的扩展程序"导入该目录即可）；
+
+- 2、在Chrome浏览器中输入[chrome://extensions](chrome://extensions)，并开启开发者模式（点击右上角）；
+
+- 3、将下载的插件文件解压后，点击"加载已解压的扩展程序"进行载入；
+
+- 4、在使用UMEM助手之前，请先登录友盟账号 & 登录[UMEM](https://github.com/Samge0/UmengEventManage.git) 账号；
+
+- 5、从浏览器右上角点击安装好的插件图标，选择目标服务器地址（[UMEM](https://github.com/Samge0/UmengEventManage.git) 项目部署的地址）+端口号（默认8000），点击【一键更新配置】，插件将自动同步友盟登录状态到 [UMEM](https://github.com/Samge0/UmengEventManage.git) 配置中，免去手动复制粘贴。
+
+## 此项目版本
+
+```
+@vue/cli 4.5.7
+yarn 1.22.10
+```
+
+## node_modules 安装
+
+```
+yarn install
+```
+
+> 如果遇到问题报错，可以查看[使用 vue-cli 创建 vue3.x 版本项目报错](https://guoqiankun.blog.csdn.net/article/details/111993759) 这篇文章
+
+## 插件开发
+
+```
+yarn run watch
+```
+
+## 插件打包
+
+```
+yarn run build
+```
+
+## 其他说明
+
+- 本项目基于 [my-vue3-plugin](https://github.com/18055975947/my-vue3-plugin) 开发而成，感谢原作者的开源。
+- 以下为 [my-vue3-plugin](https://github.com/18055975947/my-vue3-plugin) 作者关于chrome插件开发的文章：
+    - [CSDN 15000字大章带你一步一步使用Vue3开发chrome浏览器插件](https://guoqiankun.blog.csdn.net/article/details/112007833)
+    - [掘金 15000字大章带你一步一步使用Vue3开发chrome浏览器插件](https://juejin.cn/post/6912295521172324360/)
+
---
+++ src/popup/components/app.vue
@@ -0,0 +1,245 @@
+<template>
+	<div class="popup_page">
+		<p class="popup_title">UMEM助手</p>
+
+    <select
+      v-model="currToken"
+      @change="onSelectChange($event)"
+      class="popup_select"
+      placeholder="服务器"
+    >
+      <option
+        v-for="(options,domain) in domainList"
+        :key="domain"
+        :value="options.token"
+        class="popup_option"
+      >
+        {{options.domain}}
+      </option>
+    </select><br>
+
+    <input
+      v-model="currPort"
+      class="popup_input"
+      type="number"
+      placeholder="端口号，默认8000"
+    />
+
+		<button
+      @click="getCookie()"
+      class="popup_button">{{bt_txt}}
+    </button>
+
+    <p class="popup_tip" v-if="tipTxt.length>0">{{tipTxt}}</p>
+
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from 'vue'
+import axios from 'axios';
+export default defineComponent({
+  created() {
+    this.init()
+  },
+  setup() {
+    const state = reactive({
+      bt_txt: '一键更新配置',
+
+      tipTxt: '',
+
+      form: {
+        uc_user_agent: '',
+        uc_token: '',
+        uc_token_haitang: '',
+        uc_cookie: ''
+      },
+
+      currToken: '',
+      currDomain: '',
+      currPort: '8000',
+      domainList: [
+        {
+          domain: '',
+          token: ''
+        }
+      ]
+    })
+
+    /**
+     * 初始化token
+     */
+    const init = () => {
+      // 初始化
+      state.form.uc_user_agent = navigator.userAgent
+
+      // 获取token
+      chrome.cookies.getAll({name: 'UMEM_TOKEN'}, (cookies) => {
+        state.domainList = []
+        for (const c of cookies) {
+          state.domainList.push(
+            {
+              domain: c.domain,
+              token: c.value
+            }
+          )
+        }
+        if (state.domainList.length > 0) {
+          state.currDomain = state.domainList[0].domain
+          state.currToken = state.domainList[0].token
+        }
+        console.log(`初始化成功：${state.domainList}`)
+      })
+    }
+
+    /**
+     * 获取cookie
+     */
+    const getCookie = () => {
+      if (!state.domainList || state.domainList.length === 0) {
+        state.tipTxt = '未检测到UMEM页面'
+        return
+      }
+      if (!state.currDomain || state.currDomain.length === 0) {
+        state.tipTxt = '请先选择目标页面地址'
+        return
+      }
+      if (!state.currToken || state.currToken.length === 0) {
+        state.tipTxt = `请在 ${getCurrUrl()} 中登录UMEM后再操作`
+        return
+      }
+
+      state.tipTxt = ''
+      chrome.cookies.getAll({
+        domain: 'umeng.com'
+      }, (cookies) => {
+        state.form.uc_cookie = cookies.map(c => `${c.name}=${c.value}`).join(';')
+        state.form.uc_cookie.split(';').forEach((item) => {
+          if (item.search('XSRF-TOKEN-HAITANG') !== -1) {
+            state.form.uc_token_haitang = item.split('=')[1]
+          } else if (item.search('XSRF-TOKEN') !== -1) {
+            state.form.uc_token = item.split('=')[1]
+          }
+        });
+        uploadInfo();
+      })
+    }
+
+    /**
+     * 上传信息
+     */
+    function uploadInfo() {
+      const url = `${getCurrUrl()}/api/save_config`
+      axios.post(url, state.form, { headers: {'Authorization': state.currToken} })
+        .then(function (res) {
+          console.log(res);
+          if (res.data.code === 200) {
+            state.tipTxt = '更新成功'
+            chrome.tabs.query({title: 'UMEM-*'}, (tabs) => {
+              for (const tab of tabs) {
+                chrome.tabs.reload(tab.id)
+              }
+            })
+          } else {
+            state.tipTxt = `${getCurrUrl()} ${res.data.msg}：${res.data.code}`
+          }
+        })
+        .catch(function (e) {
+          console.log(e);
+          state.tipTxt = `${getCurrUrl()} 更新失败: ${e.toString()}`
+        });
+    }
+
+    /**
+     * 选择项改变时的监听
+     * @param event
+     */
+    const onSelectChange = (event) => {
+      const i = event.target.selectedIndex
+      if (state.domainList.length > i) {
+        state.currDomain = state.domainList[i].domain
+      }
+    }
+
+    /**
+     * 获取当前的服务链接信息
+     */
+    const getCurrUrl = () => {
+      return `http://${state.currDomain}:${state.currPort}`
+    }
+
+    return {
+      ...toRefs(state),
+      getCookie,
+      init,
+      onSelectChange
+    }
+  }
+})
+
+</script>
+
+<style lang="less" scoped>
+	.popup_page{
+    padding: 0px 20px 20px 20px;
+    font-size: 16px;
+    width: auto;
+		color: black;
+
+    .popup_title{
+      text-align: center;
+    }
+
+	}
+
+	.popup_select{
+    width: 180px;
+    text-align: center;
+		color: black;
+    height: 30px;
+    padding: 0px 10px;
+    margin: 0px 10px 0px 10px;
+	}
+
+	.popup_option{
+    width: 180px;
+    text-align: center;
+		color: black;
+    height: 30px;
+    padding: 0px 10px;
+    margin-top: 40px;
+	}
+
+	.popup_input{
+    width: 160px;
+    text-align: center;
+		color: black;
+    height: 30px;
+    margin: 10px 10px 0px 10px;
+    background-color: #e6e6e6;
+    border: none;
+    border-radius: 4px;
+    padding: 0px 10px;
+	}
+
+	.popup_button{
+    width: 180px;
+    text-align: center;
+		color: white;
+    height: 30px;
+    margin: 10px 10px 0px 10px;
+    background-color: #409EFF;
+    border: none;
+    border-radius: 4px;
+    padding: 0px 10px;
+	}
+
+	.popup_tip{
+    width: 180px;
+    text-align: center;
+    font-size: 12px;
+		color: #999999;
+    height: auto;
+    margin: 10px 10px 0px 10px;
+	}
+</style>
